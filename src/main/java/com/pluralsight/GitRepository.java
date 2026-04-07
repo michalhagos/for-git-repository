cbbe221f2ab1d99e64eaff9e7cbb977466e80f7b
michalhagos <mhayele3@gmail.com>
2026-04-07 12:47:32 -0500

    Created java class

---
+++ src/main/java/com/pluralsight/GitRepository.java
@@ -0,0 +1,8 @@
+package com.pluralsight;
+
+public class GitRepository {
+    public static void main(String[] args) {
+        System.out.println("Good Job");
+        // new repository on git
+    }
+}
